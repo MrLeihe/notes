50d4a363c6a150e611b7e153cccc57815f7260c1
stone <1484542698@qq.com>
2020-11-26 10:23:23 +0800

    add func

--- test.js
+++ test.js
@@ -1,19 +1,3 @@
-function each(obj, callback) {
-  if (typeof callback !== "function") {
-    throw new TypeError("callback must be a function");
-  }
+// test
 
-  if (Array.isArray(obj)) {
-    for (var i = 0; i < obj.length; i++) {
-      if (callback.call(obj[i], i, obj[i]) === false) {
-        break;
-      }
-    }
-  } else {
-    for (var key in obj) {
-      if (callback.call(obj[key], key, obj[key]) === false) {
-        break;
-      }
-    }
-  }
-}
+var isSymmetry = function () {}
